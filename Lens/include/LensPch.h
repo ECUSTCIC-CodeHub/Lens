9d6855746adcaf0f73f97db05f1374dba327c74c
wenye01 <wenye_01@outlook.com>
2025-07-29 20:07:53 +0800

    初始化

---
+++ Lens/include/LensPch.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+// WinRT
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Graphics.Capture.h>
+#include <winrt/Windows.Graphics.DirectX.Direct3d11.h>
+#include <winrt/Windows.Graphics.DirectX.h>
+#include <winrt/Windows.System.h>
+#include <winrt/Windows.UI.Composition.Desktop.h>
+#include <winrt/Windows.UI.Composition.h>
+#include <winrt/Windows.UI.h>
+#include <winrt/Windows.UI.Popups.h>
+#include <winrt/Windows.Web.Syndication.h>
+
+#include <DispatcherQueue.h>
+#include <windows.ui.composition.interop.h>
+
+
+#include <Windows.h>
---
+++ Lens/src/LensPch.cpp
@@ -0,0 +1,1 @@
+﻿#include "LensPch.h"
